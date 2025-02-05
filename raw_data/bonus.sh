0cb18fb627c555aacedd2e794d1cef8e9b977d8d
frankmusiime <f.musiime@alustudent.com>
2025-02-05 12:13:27 -0800

    A bash script that extracts top 10 highest temperatures duplication

---
+++ raw_data/bonus.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+input="/root/lab-1_sateliteDataAnalysis/analyzed_data"
+mv highest_temperatures.csv  humidity_data_Libya.csv  /done/
--- raw_data/highest_temp.sh
+++ raw_data/highest_temp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Set the input and output file paths
-
 input="/root/lab-1_sateliteDataAnalysis/raw_data/satelite_temperature_data.csv"
 output="/root/lab-1_sateliteDataAnalysis/analyzed_data/highest_temperatures.csv"
 
 # Sort temperatures in descending order, remove duplicates, and get top 10 unique temperatures
 cut -d, -f3 "$input" | sort -nr | uniq | head -n 10 > "$output"
 
-# Optional: print the result to the console for confirmation
+# Print the result to the console for confirmation
 cat "$output"
