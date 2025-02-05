3ee05af3220448a4c9fb6640e6bc66b7757f68bd
ShamiPlacide <p.igabe@alustudent.com>
2025-02-05 13:22:44 -0800

    Bash script for extracting the country and its humidity

---
+++ raw_data/humidity_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Asking the user the African country they want its humidity insights
+echo "What African country do you want to see its humidity?"
+read country_name
+
+# Paths for input and output data
+input="/root/lab-1_sateliteDataAnalysis/raw_data/satelite_temperature_data.csv"
+output="/root/lab-1_sateliteDataAnalysis/analyzed_data/humidity_data_${country_name}.csv"
+
+# Extracting rows for the country and humidity using cut and sorting in descending order
+grep "$country_name" "$input" | cut -d, -f1,4 | sort -t, -k2,2nr > "$output"
+
+echo "Filtered humidity data for $country_name has been saved to $output"
