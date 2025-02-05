0890011ab126823889b569dd57fe2a98f4eb063b
ShamiPlacide <p.igabe@alustudent.com>
2025-02-05 13:12:19 -0800

    editing humidity extracting file

--- extract2.sh
+++ extract2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo  "What country do you want to see?"
+echo  "What African country do you want to see its humidity?"
 read  country_name
 
 input="/root/lab-1_sateliteDataAnalysis/raw_data/satelite_temperature_data.csv"
@@ -8,4 +8,4 @@ output="/root/lab-1_sateliteDataAnalysis/analyzed_data/humidity_data_${country_n
 
 
 # Extract rows for any country, sort by humidity (descending), and save to output
-grep "$country_name" "$input" | sort -t, -k4,4nr > "$output"
+grep "$country_name" "$input" | cut -d, -f1,4 | sort -t, -k2,2nr > "$output"
